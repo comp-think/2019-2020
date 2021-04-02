bc84bfdec25fcb394ade7d3068070801942eef80
Silvio Peroni <essepuntato@gmail.com>
2021-04-02 12:51:10 +0200

    Update texts

--- docs/project/my_test_group.py
+++ docs/project/my_test_group.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019, Silvio Peroni <essepuntato@gmail.com>
+# Copyright (c) 2020, Silvio Peroni <essepuntato@gmail.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for any purpose
 # with or without fee is hereby granted, provided that the above copyright notice
